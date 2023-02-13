8dd11ef4dc6e44a335b7618a75c6e4e46387db52
bnaqica <bnaqica@ysgco.com>
2023-02-13 07:43:56 -0700

    Initial commit of service structure

---
+++ src/main/java/com/bnaqica/applicantcreditscore/ApplicantCreditScoreApplication.java
@@ -0,0 +1,13 @@
+package com.bnaqica.applicantcreditscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantCreditScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicantCreditScoreApplication.class, args);
+	}
+
+}
